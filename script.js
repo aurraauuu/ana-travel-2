3dd64f908d9a3387ac8a86e8cde14835a8adea2f
aurraauuu <auurrau@gmail.com>
2025-05-30 08:26:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Tambahkan logika pengiriman formulir di sini
+    alert('Pesan Anda telah terkirim!'); // Ganti dengan logika yang sesuai
+});
